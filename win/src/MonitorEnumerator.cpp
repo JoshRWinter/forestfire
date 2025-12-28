90db9ce8b1aff42ba03132d3a7897931651aac58
Josh Winter <winterjoshr@gmail.com>
2025-12-28 17:02:29 -0500

    x11 monitor enumeration

---
+++ win/src/MonitorEnumerator.cpp
@@ -0,0 +1,45 @@
+#include <win/MonitorEnumerator.hpp>
+
+#if defined WINPLAT_LINUX
+#include <win/X11MonitorEnumerator.hpp>
+#elif defined WINPLAT_WINDOWS
+#error "not implemented"
+#endif
+
+namespace win
+{
+
+MonitorEnumerator::MonitorEnumerator()
+{
+#if defined WINPLAT_LINUX
+	inner.reset(new X11MonitorEnumerator());
+#elif defined WINPLAT_WINDOWS
+#endif
+}
+
+void MonitorEnumerator::refresh()
+{
+	inner->refresh();
+}
+
+int MonitorEnumerator::count() const
+{
+	return inner->count();
+}
+
+const Monitor & MonitorEnumerator::operator[](int index) const
+{
+	return inner->operator[](index);
+}
+
+std::vector<Monitor>::const_iterator MonitorEnumerator::begin() const
+{
+	return inner->begin();
+}
+
+std::vector<Monitor>::const_iterator MonitorEnumerator::end() const
+{
+	return inner->end();
+}
+
+}
